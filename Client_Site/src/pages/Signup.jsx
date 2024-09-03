760874f87b7eb4f3d6823ec24125463fb825cc5a
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-09-03 16:47:44 +0600

    Complete Sign up ui design

--- Client_Site/src/pages/Signup.jsx
+++ Client_Site/src/pages/Signup.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>
-      <p>This is sign up</p>
+    <div className="max-w-lg mx-auto p-4">
+      <h1 className="text-4xl font-semibold text-center my-6">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          className="p-3 rounded-lg bg-slate-300"
+          type="text"
+          placeholder="Username"
+          id="usernsme"
+        />
+        <input
+          className="p-3 rounded-lg bg-slate-300"
+          type="email"
+          placeholder="Email"
+          id="email"
+        />
+        <input
+          className="p-3 rounded-lg bg-slate-300"
+          type="password"
+          placeholder="Password"
+          id="password"
+        />
+        <button className="p-3 bg-slate-700 text-white font-semibold uppercase rounded-lg hover:opacity-95">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5 font-medium">
+        <p>Have an account ?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-500">Sign In</span>
+        </Link>
+      </div>
     </div>
   );
 };
