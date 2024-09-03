f52c7d6781592b8528d2ef79e3525f711c3a04ea
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-09-03 10:58:34 +0600

    create dynamic api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import routerUser from "./routes/user.routes.js";
 dotenv.config();
-const app = express();
 
 mongoose
   .connect(process.env.MONGO)
@@ -13,6 +13,10 @@ mongoose
     console.log(err);
   });
 
+const app = express();
+
 app.listen(3000, () => {
   console.log("Server is runing from 3000 !");
 });
+
+app.use("/api/user", routerUser);
