b7337c0e6ff95394a76a1981eedae662f0d13f50
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-09-03 10:15:44 +0600

    Connect backend with mongodb by mongoose

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connect to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server is runing from 3000 !");
